6532f55070c100759c01b4cfbd56446bbc2b7a2e
skzweme <kateshaffr@gmail.com>
2024-11-15 20:39:37 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -320,7 +320,7 @@ function getRankInfo(d) {
     if (d.isEliminated) {
         return "Eliminated in Episode " + episodes[d.ranking.length - 1];
     }
-    return "Current TOP 7, Rank " + d.currentRank + " " + displayRankChange(d);
+    return "Current Contestant, Rank " + d.currentRank + " " + displayRankChange(d);
 }
 
 function updateNotes(d) {
